9552edcdd542e1f481782a4070f796a7f5b6ccd7
KovalovV <90799576+KovalovV@users.noreply.github.com>
2021-10-04 13:56:35 +0300

    Add files via upload

--- 2/task2.py
+++ 2/task2.py
@@ -1,17 +1,40 @@
-class Rational:
-    def __init__(self, numerator = 1, denominator = 2): 
-        self.__numerator = numerator  
-        self.__denominator = denominator
-    
-    # Функція знаходження найбільшого спільного дільника
-    def nsd(__numerator, __denominator):
-        while __numerator * __denominator != 0:
-            if __numerator >= __denominator:
-                __numerator = __numerator % __denominator
-            else:
-                __denominator = __denominator % __numerator
-        return __numerator + __denominator 
-    
-    #...
+import math
 
-rational = Rational()
+def notification(): 
+        print('Incorrect data')
+        exit(1)
+
+class Rational:
+    def __init__(self, numerator = 1, denominator = 2):
+        try:
+            self.__numerator = self.nsd(numerator, denominator)[0]
+            self.__denominator = self.nsd(numerator, denominator)[1]
+        except:
+            notification()
+
+    def str1(self): 
+        return str(self.__numerator) + "/" + str(self.__denominator)
+
+    def str2(self): 
+        return self.__numerator / self.__denominator
+    
+    # the method of finding the greatest common divisor
+    def nsd(self, numerator, denominator):
+        # use the gcd function from the math library
+        n_s_d = math.gcd(numerator, denominator)
+        # return the tuple
+        return numerator//n_s_d, denominator//n_s_d
+
+# def check_input(rational):
+#     try:
+#         return rational
+#     except:
+#         return None
+
+rational = Rational(8, -4)
+print(rational.str1())
+print(rational.str2())
+
+rational = Rational('5', 5)
+print(rational.str1())
+print(rational.str2())
